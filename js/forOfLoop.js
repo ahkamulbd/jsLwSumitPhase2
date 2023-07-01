66c8baf1ecd336093be785cca4f94340deafa474
ahkamulbd <ahkamulbd@gmail.com>
2023-07-01 09:13:35 +0600

    For of Loop addressed

---
+++ js/forOfLoop.js
@@ -0,0 +1,15 @@
+const name = 'emran';
+
+for (let nameLetter of name) {
+    //console.log(nameLetter);
+}
+
+const array = [3, -45, 87, 7, 20, 21, 45];
+// for (let arrayElement of array) {
+//     //console.log(arrayElement);
+// }
+
+for (let i = 0; i < array.length; i++) {
+    let index = array[i];
+    console.log(i, index);
+}
