fc6841563fcadbc6c02c95a0bb4c02c59df7724d
ahkamulbd <ahkamulbd@gmail.com>
2023-07-10 16:50:26 +0600

    Arrow Function addressed

---
+++ js/arrowFunc.js
@@ -0,0 +1,18 @@
+// function hello() {
+//     return 'Hello World';
+// }
+
+const hello = () => 'Hello World with JS';
+
+let result = hello();
+//console.log(result);
+
+const nameFunction = (name) => `My name is ${name}`;
+
+const result1 = nameFunction('Arkam');
+//console.log(result1);
+
+const addFunction = (a, b) => `Sum = ${a + b}`;;
+
+const sum = addFunction(15, 6);
+//console.log(sum);
